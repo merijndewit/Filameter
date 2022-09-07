84855ed86315cf0dbc6f502ec362ac0ee39a536b
merijndewit <merijn.de.wit@gmail.com>
2022-09-07 11:20:35 +0100

    initial commit, added a simple window

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import os
+
+root = Tk()
+
+#creating labels
+headerLabel = Label(root, text="Filameter")
+
+#positioning the labels on the screen
+headerLabel.grid(row=0, column=0)
+
+root.mainloop();
