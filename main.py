03d7e73af91ad4f3ac032a4413fec437b76ae15b
merijndewit <merijn.de.wit@gmail.com>
2022-09-16 12:42:56 +0100

    added a settings frame with toggle buttons

--- main.py
+++ main.py
@@ -78,6 +78,47 @@ class ControlPad(customtkinter.CTkFrame):
         self.subtractButton = customtkinter.CTkButton(master=self, text="-",  fg_color=parent.buttonColor, hover_color=parent.buttonHoverColor, text_font=("", 16), width=50, height=50)
         self.subtractButton.grid(row=2, column=0, padx=(10, 10), pady=(5, 10))
 
+class SettingsFrame(customtkinter.CTkFrame):
+    def __init__(self, parent, *args, **kwargs):
+        customtkinter.CTkFrame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.configure( width=150,
+                        height=200,
+                        corner_radius=4,
+                        fg_color="#1E1E1E")
+
+        self.grid(row=1, column=0, padx=(200, 30), pady=(5, 0), sticky=W)
+
+        self.selectedButton = None
+
+        self.headerLabel = customtkinter.CTkLabel(master=self, text="Settings", text_color="#ffffff" )
+        self.headerLabel.grid(row=0, column=0, padx=(2, 2), pady=(2, 0))
+
+        self.numberOfMeasurementsSetting = customtkinter.CTkButton(master=self, text="no of measurements", fg_color="#292929", hover_color="#292929", text_font=("", 11), text_color="#ffffff", width=90, height=40, command= lambda: self.Select(self.numberOfMeasurementsSetting))
+        self.numberOfMeasurementsSetting.grid(row=1, column=0, padx=(10, 10), pady=(2, 5))
+
+        self.test1 = customtkinter.CTkButton(master=self, text="test1", fg_color="#292929", hover_color="#292929", text_font=("", 11), text_color="#ffffff", width=90, height=40, command= lambda: self.Select(self.test1))
+        self.test1.grid(row=2, column=0, padx=(10, 10), pady=(2, 5))
+
+        self.test2 = customtkinter.CTkButton(master=self, text="test2", fg_color="#292929", hover_color="#292929", text_font=("", 11), text_color="#ffffff", width=90, height=40, command= lambda: self.Select(self.test2))
+        self.test2.grid(row=3, column=0, padx=(10, 10), pady=(2, 5))
+
+    def Select(self, selectedButton):
+        if selectedButton == self.selectedButton:
+            selectedButton.configure(fg_color="#292929", hover_color="#292929")
+            self.selectedButton = None
+            return
+        if self.selectedButton is not None:
+            self.selectedButton.configure(fg_color="#292929", hover_color="#292929")
+        self.selectedButton = selectedButton
+        selectedButton.configure(fg_color="#7C98B3", hover_color="#7C98B3")
+
+            
+
+        
+
+
+
 class Main(customtkinter.CTk):
     def __init__(self, *args, **kwargs):
         customtkinter.CTk.__init__(self, *args, **kwargs)
@@ -90,6 +131,7 @@ class Main(customtkinter.CTk):
         self.filamentViewFrame = FilamentViewFrame(self)
         self.buttonFrame = ButtonFrame(self)
         self.measurementFrame = ControlPad(self)
+        self.settingsFrame = SettingsFrame(self)
         self.mainloop();
 
     def TakeAndMeasureImage(self):
